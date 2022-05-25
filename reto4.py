b8c4cae5cca109463ddff8c32555f1a9ad19e32f
Sardilal <ardilaloaizasebastian@gmail.com>
2022-05-24 21:56:37 -0500

    arreglados los espacios en reto 4 y las comas

--- reto4.py
+++ reto4.py
@@ -1,8 +1,8 @@
 def ordenes(rutinaContable:list):
     totales =tuple(map(lambda lista: sum(tuple(map(lambda tupla: tupla[1]*tupla[2], lista[1:]))), rutinaContable))
    
-    print ('----------------- Inicio Registro diario --------------------------')
+    print ('------------------------ Inicio Registro diario ---------------------------------')
     for i in range(len(rutinaContable)):
-        print (f'La factura {rutinaContable[i][0]} tiene un total en pesos de {round( totales[i] if totales[i] >= 600000 else (totales[i] + 25000), 2 )}')
+        print (f'La factura {rutinaContable[i][0]} tiene un total en pesos de {round( totales[i] if totales[i] >= 600000 else (totales[i] + 25000), 2 ),}')
     
-    print('----------------- Fin Registro diario -----------------------------')
+    print('-------------------------- Fin Registro diario ----------------------------------')
